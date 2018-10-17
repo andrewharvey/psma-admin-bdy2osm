8d4f00cde66f833426703984974c81d13ec51a32
Andrew Harvey <andrew.harvey4@gmail.com>
2018-10-17 18:04:45 +1100

    roundtrip to clean up topology after simplification

--- simplify.sh
+++ simplify.sh
@@ -4,4 +4,9 @@ mkdir -p topojson-simplified
 for state in {1..9}; do
     echo $state
     node --max_old_space_size=4096 /usr/bin/toposimplify --filter-all --filter-detached --spherical-area 0.0000000000001 -o topojson-simplified/${state}.topojson topojson/${state}.topojson
+
+    # roundtrip to remove duplicate arcs and join adjancent arcs only used once https://github.com/topojson/topojson-simplify/issues/4
+    topo2geo < topojson-simplified/${state}.topojson LGA=topojson-simplified/LGA_${state}.json SL=topojson-simplified/SL_${state}.json
+    geo2topo LGA=topojson-simplified/LGA_${state}.json SL=topojson-simplified/SL_${state}.json > topojson-simplified/${state}.topojson
+    rm topojson-simplified/LGA_${state}.json topojson-simplified/SL_${state}.json
 done
