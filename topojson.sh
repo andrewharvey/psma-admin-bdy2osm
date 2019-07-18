e15dae5080def99639efdb40253a1e06dd1f49af
Andrew Harvey <andrew@alantgeo.com.au>
2019-07-18 21:39:37 +1000

    upgrade to JUNE 2019

--- topojson.sh
+++ topojson.sh
@@ -3,5 +3,5 @@
 mkdir -p topojson
 for state in {1..9}; do
     echo $state
-    node --max_old_space_size=4096 /usr/bin/geo2topo -q0 -o topojson/${state}.topojson LGA=geojson_osm/${state}_LGA_OSM.geojson SL=geojson_osm/${state}_SL_OSM.geojson
+    node --max_old_space_size=4096 ./node_modules/.bin/geo2topo -q0 -o topojson/${state}.topojson LGA=geojson_osm/${state}_LGA_OSM.geojson SL=geojson_osm/${state}_SL_OSM.geojson
 done
